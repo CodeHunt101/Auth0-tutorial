d78cca66e7b3473ccdec06818689aa221dc415cb
haroldtm <harold.torres@lexicondigital.com.au>
2022-06-17 17:53:57 +1000

    Auth0 tutorial

--- pages/signIn.js
+++
@@ -1,9 +0,0 @@
-const SignIn = () => {
-  return (
-    <a target="_blank" href="https://dev-haroldtm.au.auth0.com/authorize?response_type=code&client_id=dEnK3ppb1E2mGcaHoe24S6v23Y9lX66T&redirect_uri=http://localhost:3000/callback&scope=openid">
-      Log in
-    </a>
-  )
-}
-
-export default SignIn
